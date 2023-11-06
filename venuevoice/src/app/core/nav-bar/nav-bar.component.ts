590b4313fe548300028e17dc2aca310645a2d2b6
blake-gifford <blakegifford01@gmail.com>
2023-11-06 14:09:07 -0700

    adding the start of pkce auth flow

--- venuevoice/src/app/core/nav-bar/nav-bar.component.ts
+++ venuevoice/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,17 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
+import { Component, OnInit } from '@angular/core';
+import { sha256 } from 'js-sha256'; // You may need to install this library or another to perform SHA-256 hashing
+import { base64url } from 'rfc4648';
+import { AuthServiceService } from 'src/app/auth-service.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  topTracks: any;
 
-  constructor(private http: HttpClient,
-    private router: Router) { }
+  constructor(private authService: AuthServiceService) { }
 
+  ngOnInit(): void {
+    this.authService.getUserTopTracks()
+      .then(tracks => {
+        this.topTracks = tracks;
+        console.log(this.topTracks);
+      })
+      .catch(error => {
+        console.error('Error fetching top tracks:', error);
+      });
+  }
+
+  login() {
+    this.authService.initiateAuthFlow();
+    console.log("Login");
+  }
 
 }
